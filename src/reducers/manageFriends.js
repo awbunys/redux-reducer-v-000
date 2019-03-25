83b0dd015a61e7a96269b25fcb8023a40aa293dc
Andrew Bunys <awbunys@gmail.com>
2019-03-25 23:11:08 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  if (action.type === 'ADD_FRIEND') {
+    return {
+      friends: ['why isnt this working']
+    }
+  }
+  if (action.type !== action) {
+    return {
+      ...state
+    };
+  }
 }
