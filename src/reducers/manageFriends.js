077e466a630f19d07f4c092165c1467ae10adda9
Andrew Bunys <awbunys@gmail.com>
2019-03-26 00:32:48 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,11 +1,12 @@
 export function manageFriends(state, action){
   if (action.type === 'REMOVE_FRIEND') {
-    let index = state.friends.findIndex(x => x.id === action.id)
     let newArr = [...state.friends]
+    let index = newArr.findIndex(x => x.id === action.id)
+
     (function () {
     newArr.splice(index, 1)
 })();
-    return newArr
+    return index
   }
   if (action.type === 'ADD_FRIEND') {
     return {
